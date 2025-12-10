6cf01dc23d166a78a357ac9d8444e2db458d575a
Raunak Singh <raunaksingh9800@gmail.com>
2025-12-10 14:51:15 +0530

    f

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,14 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
+  ...nextCoreWebVitals,
+  ...nextTypescript,
   // 👇 Ignore generated files and others
   {
     ignores: [
@@ -27,7 +24,7 @@ const eslintConfig = [
       "src/app/dashboard/ai/page.tsx",
       "src/app/dashboard/ai/Ai.tsx"
     ],
-  },
+  }
 ];
 
 export default eslintConfig;
